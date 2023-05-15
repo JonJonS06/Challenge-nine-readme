ff616621e750b981ed1b38375b432ceae5037ea1
JonJonS06 <124073266+JonJonS06@users.noreply.github.com>
2023-05-15 15:29:04 -0400

    added user questions

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  'What is your project title?', 
+  'Please give a short description of you project.', 
+  'What are the installation instructions?', 
+  'Please include usage information.', 
+  'Who contributed on this project?', 
+  'What tests were run on this project?',
+  'Please select a license for this project from the list.'];
+
+  for (const question of questions) {
+    console.log(question);
+  }
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(README, data) {
+  
+}
 
 // TODO: Create a function to initialize app
 function init() {}
