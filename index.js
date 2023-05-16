bdda67b3ccb065a3a761b8ffdff65fb1cb61603b
JonJonS06 <124073266+JonJonS06@users.noreply.github.com>
2023-05-16 10:54:20 -0400

    updated questions/ added functio to write file/ created the function to initialize the file

--- index.js
+++ index.js
@@ -1,35 +1,88 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+// const generateMarkdown = require('./utils/generateMarkdown');
+const path = require('path');
+
 
 // TODO: Create an array of questions for user input
-const questions = [
-  'What is your project title?', 
-  'Please give a short description of you project.', 
-  'What are the installation instructions?', 
-  'Please include usage information.', 
-  'Who contributed on this project?', 
-  'What tests were run on this project?',
-  'Please select a license for this project from the list.'];
-
-  for (const question of questions) {
-    console.log(question);
-  }
+const questions = [ 
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?'
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: "What is your project's name?",
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please write a short description of your project.',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What kind of license should your project have?',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What command should be run to install dependencies?',
+    default: 'npm i',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'What command should be run to run tests?',
+    default: 'npm test',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What does the user need to know about using the repository?',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'What does the user need to know about contributing to the repository?',
+  },
+];
+  
+  
 
 // TODO: Create a function to write README file
-function writeToFile(README, data) {
-  
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), filename), data);
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((inquirerResponses) => {
+    console.log('Generating ReadMe');
+    writeToFile('ReadMe.md', generateMarkdown({...inquirerResponses}));
+  });
+}
 
 // Function call to initialize app
 init();
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'None')  {
+    return `![Github License](https://img.shields.io/badge/license-${license}-blue.svg)`
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
